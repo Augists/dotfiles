133fbf81b6d45e1975fcf1a9249a23b141290b30
Augists <awzyc2010@163.com>
2021-08-18 10:04:32 +0800

    Dotfiles version 2.0

---
+++ .config/nnn/nnn_conf.zsh
@@ -0,0 +1,10 @@
+export PATH="$PATH:$HOME/.config/nnn/bin"
+
+export NNN_BMS="e:$HOME/.config/;d:$HOME/Downloads/;c:~/Downloads/.code/"
+export NNN_COLORS='4321'
+export NNN_FIFO='/tmp/nnn.fifo'
+export NNN_TMPFILE='/tmp/.lastd'
+export NNN_SEL='/tmp/.sel'
+
+alias nnn='nnn -dHe'
+alias nd='source /tmp/.lastd'
