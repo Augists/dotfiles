9e0ffb9f18db516afd6383204340bb4a19220d2f
Augists <awzyc2010@163.com>
2021-11-10 00:16:14 +0800

    * Remove security: leak DLUT-EDA network account* Use cht.sh

--- .config/zsh/network.zsh
+++ .config/zsh/network.zsh
@@ -1,6 +1,6 @@
+alias proxy_on_C='export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890'
 alias proxy_on_U='export all_proxy=socks5://127.0.0.1:1080'
 alias proxy_off='unset all_proxy'
-alias proxy_on_C='export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890'
 # export http_proxy=socks5://127.0.0.1:1080
 # export https_proxy=socks5://127.0.0.1:1080
 #
@@ -8,7 +8,7 @@ alias proxy_on_C='export https_proxy=http://127.0.0.1:7890 http_proxy=http://127
 alias git_proxy_on='git config --global http.proxy "http://127.0.0.1:1080"; git config --global https.proxy "https://127.0.0.1:1080"'
 alias git_proxy_off='git config --global --unset http.proxy; git config --global --unset https.proxy'
 
-alias eda='~/Downloads/GitHub/dlut-eda-login/dlut_eda_login.sh -u "201992222" -p "06133017"'
+alias eda='source /Users/zhangyichi/.config/zsh/network.sh'
 alias eda_out='~/Downloads/GitHub/dlut-eda-login/dlut_eda_login.sh -o'
 
 alias dv='networksetup -setairportnetwork en0 We_R_Family_5G'
