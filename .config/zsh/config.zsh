b3214ed8dfd935194c388c26245eb9b321d6d9ee
Augists <awzyc2010@163.com>
2022-08-15 18:19:32 +0800

    lf

--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -5,7 +5,6 @@ source ${CONFIG_DIR}/nnn/nnn_conf.zsh
 
 source ${ZSH_CONFIG_DIR}/network.zsh
 source ${ZSH_CONFIG_DIR}/fzf.zsh
-source ${ZSH_CONFIG_DIR}/python.zsh
 
 alias cht.sh="$ZSH_CONFIG_DIR/cht.sh"
 
--- .config/zsh/python.zsh
+++
@@ -1,1 +0,0 @@
-export PATH="$PATH:/Users/zhangyichi/Library/Python/3.8/bin"
