46a0bce0aea7e0fd19fea2aa07f76efa7047a736
Augists <awzyc2010@163.com>
2022-10-16 20:28:44 +0800

    remove llvm

--- .vim/vim_conf.zsh
+++ .vim/vim_conf.zsh
@@ -1,8 +1,8 @@
 alias vim='nvim'
 alias luamake=/Users/zhangyichi/.config/lua-language-server/3rd/luamake/luamake
 export PATH="$PATH:$HOME/.config/lua-language-server/bin/macOS"
-export CPPFLAGS="-I/usr/local/opt/llvm/include"
-export LDFLAGS="-L/usr/local/opt/llvm/lib"
-export PATH="$PATH:/usr/local/opt/llvm/bin"
+# export CPPFLAGS="-I/usr/local/opt/llvm/include"
+# export LDFLAGS="-L/usr/local/opt/llvm/lib"
+# export PATH="$PATH:/usr/local/opt/llvm/bin"
 # LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
 export PATH="$PATH:$HOME/.local/bin"
