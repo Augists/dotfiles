c2d71d95a1948d20f8dda8fdfaf80388246e8a8d
Augists <awzyc2010@163.com>
2024-02-06 01:56:46 +0800

    remove dlut network

--- .config/zsh/network.zsh
+++ .config/zsh/network.zsh
@@ -8,7 +8,4 @@ alias proxy_off='unset all_proxy'
 alias git_proxy_on='git config --global http.proxy "http://127.0.0.1:1080"; git config --global https.proxy "https://127.0.0.1:1080"'
 alias git_proxy_off='git config --global --unset http.proxy; git config --global --unset https.proxy'
 
-alias eda='source /Users/zhangyichi/.config/zsh/network.sh'
-alias eda_out='~/Downloads/GitHub/dlut-eda-login/dlut_eda_login.sh -o'
-
 alias dv='networksetup -setairportnetwork en0 We_R_Family_5G'
